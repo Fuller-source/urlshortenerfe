a88e8803b5986bd635c09b68b330f0e950bc32f3
James Fuller <fuller.jdf@outlook.com>
2025-09-27 20:00:57 -0500

    Link backend

--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(req: NextRequest){
     const requestText = await req.text();
-    const urlShortenBE = 'http://localhost:8080/shorten' // This will need to change
+    const urlShortenBE = 'https://url-shortener-api-89bq.onrender.com/shorten' // This will need to change
     
     const response = await fetch(urlShortenBE, {
         method: 'POST',
